9f215c553f972bbebee82a4fee787c5ee085dbf9
hecmung <kenwey3018@gmail.com>
2023-06-01 17:51:29 -0600

    UserService

--- src/main/java/com/crud/demo/service/UserService.java
+++ src/main/java/com/crud/demo/service/UserService.java
@@ -1,2 +1,18 @@
-package com.crud.demo.service;public class UserService {
+package com.crud.demo.service;
+
+import com.crud.demo.repositoy.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+
 }
