0f1d3b5536058b593f6b5984a1185c27eae4d3f0
hecmung <kenwey3018@gmail.com>
2023-06-01 17:49:59 -0600

    UserRepository

--- src/main/java/com/crud/demo/repositoy/UserRepository.java
+++ src/main/java/com/crud/demo/repositoy/UserRepository.java
@@ -1,2 +1,9 @@
-package com.crud.demo.repositoy;public interface UserRepository {
+package com.crud.demo.repositoy;
+
+import com.crud.demo.model.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, Long> {
 }
